848741c58caa7daa8e4350645857450777cc46b9
Willams Sousa <osdeving@gmail.com>
2023-12-09 08:47:11 -0300

    muda http para express

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
-import http from "http";
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const routes = {
-    "/": "Curso de Express API",
-    "/livros" : "Entrei na rota livros",
-    "/autores" : "Entrei na rota autores",
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200,{ 'content-type' : 'text/plain'} );
-    res.end(routes[req.url]);
-});
-
-server.listen(PORT, () => console.log(`server listening on port ${PORT}...`));
+app.listen(PORT, () => console.log(`server listening on port ${PORT}...`));
